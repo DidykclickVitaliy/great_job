c961e823d050cc9e483fc42df19a37d694abecb4
DidykclickVitaliy <iiceituice@gmail.com>
2023-02-05 21:46:16 +0200

    created a helper function to work with classes

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,13 @@ import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
 import { useTheme } from "./theme/useTheme";
 
 import "./styles/index.scss";
+import { classNames } from "./helpers/classNames/classNames";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme} `}>
+    <div className={classNames("app", {}, [theme])}>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
       <button onClick={toggleTheme}>Change theme</button>
