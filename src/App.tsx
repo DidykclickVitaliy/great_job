029abf1f474f11c71902fc828e78a06de12093f2
DidykclickVitaliy <iiceituice@gmail.com>
2023-02-05 18:11:03 +0200

    tweaked router, added code splitting using the standard React mechanism

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { Suspense } from "react";
+import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import { Counter } from "./components/Counter";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
+
 import "./index.scss";
 
 export const App = () => {
   return (
     <div className="app">
-      qweqweqwe
+      <Link to="/">Main</Link>
+      <Link to="/about">About</Link>
       <Counter />
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageLazy />} />
+          <Route path="/about" element={<AboutPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
