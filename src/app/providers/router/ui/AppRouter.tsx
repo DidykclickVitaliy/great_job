01db36c64090afa29e3407c383c5f7fafe706637
DidykclickVitaliy <iiceituice@gmail.com>
2023-03-04 13:17:47 +0200

    added custom page loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,22 +1,22 @@
 import { FC, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
+import { PageLoader } from "features/ui/PageLoader";
 import { routeConfig } from "../config/routeConfig";
 
 export const AppRouter: FC = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({ path, element }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={(
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <div className="page-wrapper">{element}</div>
-                        </Suspense>
-                    )}
-                />
-            ))}
-        </Routes>
-    </Suspense>
+    <Routes>
+        {Object.values(routeConfig).map(({ path, element }) => (
+            <Route
+                key={path}
+                path={path}
+                element={(
+                    <Suspense fallback={<PageLoader />}>
+                        <div className="page-wrapper">{element}</div>
+                    </Suspense>
+                )}
+            />
+        ))}
+    </Routes>
+
 );
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+
+import { classNames } from "shared/lib/classNames";
+
+import cls from "./Loader.module.scss";
+
+interface LoaderProps {
+  className?: string;
+}
+
+export const Loader: FC<LoaderProps> = ({ className }) => (
+    <div className={classNames(cls.Loader, {}, [className])}>
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+    </div>
+);
